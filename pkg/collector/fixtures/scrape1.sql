db7694a2f69cb3e0c65e0c2543630fb2d5d55055
Stefan Majewsky <stefan.majewsky@sap.com>
2017-03-16 10:21:01 +0100

    fix tests

--- pkg/collector/fixtures/scrape1.sql
+++ pkg/collector/fixtures/scrape1.sql
@@ -1,10 +1,10 @@
 INSERT INTO domains (id, cluster_id, name, uuid) VALUES (1, 'cluster-id-test', 'Default', '2131d24fee484da9be8671aa276360e0');
 
-INSERT INTO domain_services (id, domain_id, name) VALUES (1, 1, 'compute');
+INSERT INTO domain_services (id, domain_id, type) VALUES (1, 1, 'compute');
 
 INSERT INTO projects (id, domain_id, name, uuid) VALUES (1, 1, 'foo', 'dd53fc9c38d740c6b7889424e740e194');
 
-INSERT INTO project_services (id, project_id, name, scraped_at, stale) VALUES (1, 1, 'compute', 1, FALSE);
+INSERT INTO project_services (id, project_id, type, scraped_at, stale) VALUES (1, 1, 'compute', 1, FALSE);
 
 INSERT INTO project_resources (service_id, name, quota, usage, backend_quota) VALUES (1, 'cores', 0, 37, 100);
 INSERT INTO project_resources (service_id, name, quota, usage, backend_quota) VALUES (1, 'instances', 0, 12, 20);
