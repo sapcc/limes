e81b4d1a23ead6af6e3d21de326856fe602244ef
Stefan Majewsky <stefan.majewsky@sap.com>
2017-05-10 14:45:20 +0200

    fix recognition of HTTP(S)_PROXY environment variables with LIMES_INSECURE=1

--- pkg/util/hacks.go
+++ pkg/util/hacks.go
@@ -33,8 +33,9 @@ func init() {
 	//"DEBUG" variable. "DEBUG" is meant to be useful for production systems,
 	//where you definitely don't want to turn off certificate verification.)
 	if os.Getenv("LIMES_INSECURE") == "1" {
-		http.DefaultClient.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{
+			InsecureSkipVerify: true,
 		}
+		http.DefaultClient.Transport = http.DefaultTransport
 	}
 }
