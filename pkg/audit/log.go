c63697ef864028bd4f2b1c114e3761085247a132
Nathan Oyler <notque@gmail.com>
2018-08-07 15:17:29 -0700

    My take on the structure of limes audit message

--- pkg/audit/log.go
+++ pkg/audit/log.go
@@ -45,101 +45,106 @@ var observerUUID string
 //Trail is a list of CADF formatted events with log level AUDIT. It has a separate interface
 //from the rest of the logging to allow to withhold the logging until DB changes are committed.
 type Trail struct {
-	events []CADFevent
+	events []CADFEvent
 }
 
-//CADFevent is a substructure of Trail containing data for a CADF event (read: quota change)
-//regarding some resource in a domain or project.
-type CADFevent struct {
-	TypeURI     string         `json:"typeURI"`
-	ID          string         `json:"id"`
-	EventTime   string         `json:"eventTime"`
-	EventType   string         `json:"eventType"`
-	Action      string         `json:"action"`
-	Outcome     string         `json:"outcome"`
-	Reason      EventReason    `json:"reason"`
-	Initiator   EventInitiator `json:"initiator"`
-	Target      EventTarget    `json:"target"`
-	Observer    EventObserver  `json:"observer"`
-	RequestPath string         `json:"requestPath"`
+// CADFEvent contains the CADF event according to CADF spec, section 6.6.1 Event (data)
+// Extensions: requestPath (OpenStack, IBM), initiator.project_id/domain_id
+type CADFEvent struct {
+	TypeURI     string       `json:"typeURI"`
+	ID          string       `json:"id"`
+	EventTime   string       `json:"eventTime"`
+	EventType   string       `json:"eventType"`
+	Action      string       `json:"action"`
+	Outcome     string       `json:"outcome"`
+	Reason      Reason       `json:"reason,omitempty"`
+	Initiator   Resource     `json:"initiator"`
+	Target      Resource     `json:"target"`
+	Observer    Resource     `json:"observer"`
+	Attachments []Attachment `json:"attachments,omitempty"`
+	// requestPath is an extension of OpenStack's pycadf which is supported by IBM as well
+	RequestPath string `json:"requestPath,omitempty"`
 }
 
-//EventObserver is a substructure of CADFevent containing data for the event's observer.
-type EventObserver struct {
+// Resource contains attributes describing a (OpenStack-) Resource
+type Resource struct {
+	TypeURI   string `json:"typeURI"`
+	Name      string `json:"name,omitempty"`
+	Domain    string `json:"domain,omitempty"`
+	ID        string `json:"id"`
+	Addresses []struct {
+		URL  string `json:"url"`
+		Name string `json:"name,omitempty"`
+	} `json:"addresses,omitempty"`
+	Host        Host       `json:"host,omitempty"`
+	Attachments Attachment `json:"attachments,omitempty"`
+	// project_id and domain_id are OpenStack extensions (introduced by Keystone and keystone(audit)middleware)
+	ProjectID string `json:"project_id,omitempty"`
+	DomainID  string `json:"domain_id,omitempty"`
+}
+
+// Attachment contains self-describing extensions to the event
+type Attachment struct {
+	// Note: name is optional in CADF spec. to permit unnamed attachments
+	Name    string `json:"name,omitempty"`
 	TypeURI string `json:"typeURI"`
-	Name    string `json:"name"`
-	ID      string `json:"id"`
+	Content string `json:"content"`
 }
 
-//EventInitiator is a substructure of CADFevent containing data for the event's initiator.
-type EventInitiator struct {
-	TypeURI   string             `json:"typeURI"`
-	Name      string             `json:"name"`
-	ID        string             `json:"id"`
-	Domain    string             `json:"domain,omitempty"`
-	DomainID  string             `json:"domain_id,omitempty"`
-	ProjectID string             `json:"project_id,omitempty"`
-	Host      EventInitiatorHost `json:"host"`
+//Reason is a substructure of CADFevent containing data for the event outcome's reason.
+type Reason struct {
+	ReasonType string `json:"reasonType"`
+	ReasonCode string `json:"reasonCode"`
 }
 
 //EventInitiatorHost is a substructure of eventInitiator containing data for
 // the event initiator's host.
-type EventInitiatorHost struct {
-	Address string `json:"address"`
-	Agent   string `json:"agent"`
-}
-
-//EventTarget is a substructure of CADFevent containing data for the event's target.
-type EventTarget struct {
-	TypeURI  string     `json:"typeURI"`
-	ID       string     `json:"id"`
-	OldQuota uint64     `json:"oldQuota"`
-	NewQuota uint64     `json:"newQuota"`
-	Unit     limes.Unit `json:"unit,omitempty"`
-}
-
-//EventReason is a substructure of CADFevent containing data for the event outcome's reason.
-type EventReason struct {
-	Type string `json:"reasonType"`
-	Code string `json:"reasonCode"`
+type Host struct {
+	ID       string `json:"id,omitempty"`
+	Address  string `json:"address,omitempty"`
+	Agent    string `json:"agent,omitempty"`
+	Platform string `json:"platform,omitempty"`
 }
 
 //NewEvent takes the necessary parameters from an API request and returns a new audit event.
 func NewEvent(
 	t *gopherpolicy.Token, r *http.Request, requestTime, targetID,
 	srvType, resName string, resUnit limes.Unit, resQuota, newQuota uint64,
-) CADFevent {
-	return CADFevent{
+) CADFEvent {
+	return CADFEvent{
 		TypeURI:   "http://schemas.dmtf.org/cloud/audit/1.0/event",
 		ID:        generateUUID(),
 		EventTime: requestTime,
 		EventType: "activity",
 		Action:    "update",
 		Outcome:   "success",
-		Reason: EventReason{
-			Type: "HTTP",
-			Code: "200",
+		Reason: Reason{
+			ReasonType: "HTTP",
+			ReasonCode: "200",
 		},
-		Initiator: EventInitiator{
+		Initiator: Resource{
 			TypeURI:   "service/security/account/user",
 			Name:      t.Context.Auth["user_name"],
 			ID:        t.Context.Auth["user_id"],
 			Domain:    t.Context.Auth["domain_name"],
 			DomainID:  t.Context.Auth["domain_id"],
 			ProjectID: t.Context.Auth["project_id"],
-			Host: EventInitiatorHost{
+			Host: Host{
 				Address: TryStripPort(r.RemoteAddr),
 				Agent:   r.Header.Get("User-Agent"),
 			},
 		},
-		Target: EventTarget{
-			TypeURI:  fmt.Sprintf("service/%s/%s/quota", srvType, resName),
-			ID:       targetID,
-			OldQuota: resQuota,
-			NewQuota: newQuota,
-			Unit:     resUnit,
+		Target: Resource{
+			TypeURI: fmt.Sprintf("service/%s/%s/quota", srvType, resName),
+			ID:      targetID,
+			ProjectID: targetID,
+			Attachments: Attachment{
+				Name:    "payload",
+				TypeURI: "mime:application/json",
+				Content: fmt.Sprintf("OldQuota:%d,NewQuota:%d,Unit:%s", resQuota, newQuota, resUnit),
+			},
 		},
-		Observer: EventObserver{
+		Observer: Resource{
 			TypeURI: "service/resources",
 			Name:    "limes",
 			ID:      observerUUID,
@@ -149,7 +154,7 @@ func NewEvent(
 }
 
 //Add adds an event to the audit trail.
-func (t *Trail) Add(event CADFevent) {
+func (t *Trail) Add(event CADFEvent) {
 	t.events = append(t.events, event)
 }
 
