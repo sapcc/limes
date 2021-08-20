a56b5f7572c5f0affd36b611239f2e0c931b491f
Stefan Majewsky <stefan.majewsky@sap.com>
2021-08-20 11:06:27 +0200

    add logging for backend API calls that take longer than a minute
    Similar to the logging for scrapes that take longer than 5 minutes
    total, but with more granularity to help better debug those overlong
    scrapes.

--- pkg/util/http.go
+++ pkg/util/http.go
@@ -23,6 +23,9 @@ import (
 	"crypto/tls"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/sapcc/go-bits/logg"
 )
 
 func init() {
@@ -36,6 +39,25 @@ func init() {
 		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{
 			InsecureSkipVerify: true,
 		}
-		http.DefaultClient.Transport = http.DefaultTransport
 	}
+
+	http.DefaultClient.Transport = loggingRoundTripper{http.DefaultTransport}
+}
+
+//loggingRoundTripper adds logging for long round trips to http.RoundTripper.
+//This is used to provide visibility into slow backend API calls.
+type loggingRoundTripper struct {
+	Inner http.RoundTripper
+}
+
+func (rt loggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	start := time.Now()
+	resp, err := rt.Inner.RoundTrip(req)
+	duration := time.Since(start)
+
+	if err == nil && duration > 1*time.Minute {
+		logg.Info("API call has taken excessively long (%s): %s", duration.String(), req.URL.String())
+	}
+
+	return resp, err
 }
