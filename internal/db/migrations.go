4db1864a17f54dd240aa23e2977e22425c9dd276
VoigtS <s.voigt@sap.com>
2024-07-26 13:52:31 +0200

    Remove leftover migration from last conflict cleanup

--- internal/db/migrations.go
+++ internal/db/migrations.go
@@ -260,10 +260,6 @@ var sqlMigrations = map[string]string{
 		ALTER TABLE project_services
 			ADD COLUMN quota_sync_duration_secs REAL NOT NULL DEFAULT 0;
 	`,
-	"044_add_unique_key_to_transfer_token.down.sql": `
-		ALTER TABLE project_commitments
-			DROP CONSTRAINT project_commitments_transfer_token_key;
-	`,
 	"044_add_unique_key_to_transfer_token.up.sql": `
 		CREATE UNIQUE INDEX on project_commitments (transfer_token)
             WHERE transfer_token <> '';
