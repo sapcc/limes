4b6c9f4767fabd8d6171ba95367cc0a4e5478ee4
Stefan Majewsky <stefan.majewsky@sap.com>
2024-08-12 14:39:33 +0200

    parse quota-overrides using API-level resource renaming
    It is useful for us to use the API-level names here because:
    
    1. some quota overrides are shared between all clusters (so renaming
       must be carefully coordinated here, same as for the API), and
    2. this config file is maintained by a wider operator audience (so any
       renaming here should be properly communicated with advance notice).

---
+++ internal/core/cluster_test.go
@@ -0,0 +1,110 @@
+/*******************************************************************************
+*
+* Copyright 2024 SAP SE
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You should have received a copy of the License along with this
+* program. If not, you may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+
+// NOTE: This needs to be in a separate package to avoid an import cycle when
+// importing from internal/test.
+package core_test
+
+import (
+	"testing"
+
+	"github.com/sapcc/go-api-declarations/limes"
+	limesresources "github.com/sapcc/go-api-declarations/limes/resources"
+	"github.com/sapcc/go-bits/assert"
+
+	"github.com/sapcc/limes/internal/test"
+)
+
+const (
+	testQuotaOverridesNoRenamingConfigYAML = `
+		availability_zones: [ az-one, az-two ]
+		discovery:
+			method: --test-static
+		services:
+			- service_type: first
+				type: --test-generic
+			- service_type: second
+				type: --test-generic
+	`
+
+	testQuotaOverridesWithRenamingConfigYAML = `
+		availability_zones: [ az-one, az-two ]
+		discovery:
+			method: --test-static
+		services:
+			- service_type: first
+				type: --test-generic
+			- service_type: second
+				type: --test-generic
+		resource_behavior:
+		- resource: first/capacity
+			identity_in_v1_api: capacities/first
+		- resource: first/things
+			identity_in_v1_api: things/first
+	`
+)
+
+var expectedQuotaOverrides = map[string]map[string]map[limes.ServiceType]map[limesresources.ResourceName]uint64{
+	"firstdomain": {
+		"firstproject": {
+			"first": {
+				"capacity": 10,
+				"things":   20,
+			},
+			"second": {
+				"capacity": 30,
+				"things":   40,
+			},
+		},
+		"secondproject": {
+			"first": {
+				"capacity": 50,
+			},
+			"second": {
+				"capacity": 60,
+			},
+		},
+	},
+	"seconddomain": {
+		"thirdproject": {
+			"first": {
+				"things": 70,
+			},
+			"second": {
+				"things": 80,
+			},
+		},
+	},
+}
+
+func TestQuotaOverridesWithoutResourceRenaming(t *testing.T) {
+	t.Setenv("LIMES_QUOTA_OVERRIDES_PATH", "fixtures/quota-overrides-no-renaming.json")
+	s := test.NewSetup(t,
+		test.WithConfig(testQuotaOverridesNoRenamingConfigYAML),
+	)
+	assert.DeepEqual(t, "quota overrides", s.Cluster.QuotaOverrides, expectedQuotaOverrides)
+}
+
+func TestQuotaOverridesWithResourceRenaming(t *testing.T) {
+	t.Setenv("LIMES_QUOTA_OVERRIDES_PATH", "fixtures/quota-overrides-with-renaming.json")
+	s := test.NewSetup(t,
+		test.WithConfig(testQuotaOverridesWithRenamingConfigYAML),
+	)
+	assert.DeepEqual(t, "quota overrides", s.Cluster.QuotaOverrides, expectedQuotaOverrides)
+}
