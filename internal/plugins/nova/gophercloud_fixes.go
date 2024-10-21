sapcc/limes

commit 2fbae7df400a9c5c09d5db0c07d5d7b99eabf0e5

plugins: simplify Nova flavor listing (#579)
* plugins: simplify Nova flavor listing

In Nova microversion 2.61, flavors.ListDetail() includes the extra
specs, so we can save a ton of work querying them one flavor at a time.
Gophercloud already has support for this, too.

* fix hypervisor listing

By increasing the Nova API microversion in the previous commit, the
hypervisor "id" field has changed from a number to a UUID. We are not
using this value anywhere, but whatever.

internal/plugins/nova/hypervisor_selection.go
@@ -258,7 +258,7 @@ func (h MatchingHypervisor) PartialCapacity() PartialCapacity {
 // In our clusters, that type breaks because some hypervisors report unexpected
 // NULL values on fields that we are not even interested in.
 type Hypervisor struct {
-	ID                 int    `json:"id"`
+	ID                 string `json:"id"`
 	HypervisorHostname string `json:"hypervisor_hostname"`
 	HypervisorType     string `json:"hypervisor_type"`
 	// LocalGB            uint64              `json:"local_gb"`

internal/plugins/capacity_nova.go
@@ -29,6 +29,7 @@ import (
 
 	"github.com/gophercloud/gophercloud/v2"
 	"github.com/gophercloud/gophercloud/v2/openstack"
+	"github.com/gophercloud/gophercloud/v2/openstack/compute/v2/flavors"
 	"github.com/gophercloud/gophercloud/v2/openstack/compute/v2/servers"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sapcc/go-api-declarations/limes"
@@ -90,6 +91,8 @@ func (p *capacityNovaPlugin) Init(ctx context.Context, provider *gophercloud.Pro
 	if err != nil {
 		return err
 	}
+	p.NovaV2.Microversion = "2.61" // to include extra specs in flavors.ListDetail()
+
 	p.PlacementV1, err = openstack.NewPlacementV1(provider, eo)
 	if err != nil {
 		return err
@@ -120,17 +123,17 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 	// enumerate matching flavors, divide into split and pooled flavors;
 	// also, for the pooled instances capacity, we need to know the max root disk size on public pooled flavors
 	var (
-		splitFlavors    []nova.FullFlavor
+		splitFlavors    []flavors.Flavor
 		maxRootDiskSize = uint64(0)
 	)
-	err = p.FlavorSelection.ForeachFlavor(ctx, p.NovaV2, func(f nova.FullFlavor) error {
-		if isSplitFlavorName[f.Flavor.Name] {
+	err = p.FlavorSelection.ForeachFlavor(ctx, p.NovaV2, func(f flavors.Flavor) error {
+		if isSplitFlavorName[f.Name] {
 			splitFlavors = append(splitFlavors, f)
-		} else if f.Flavor.IsPublic {
+		} else if f.IsPublic {
 			// only public flavor contribute to the `maxRootDiskSize` calculation (in
 			// the wild, we have seen non-public legacy flavors with wildly large
 			// disk sizes that would throw off all estimates derived from this number)
-			maxRootDiskSize = max(maxRootDiskSize, liquids.AtLeastZero(f.Flavor.Disk))
+			maxRootDiskSize = max(maxRootDiskSize, liquids.AtLeastZero(f.Disk))
 		}
 		return nil
 	})
@@ -176,7 +179,7 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 
 	demandByFlavorName := make(map[string]liquid.ResourceDemand)
 	for _, f := range splitFlavors {
-		resourceName := p.FlavorAliases.LimesResourceNameForFlavor(f.Flavor.Name)
+		resourceName := p.FlavorAliases.LimesResourceNameForFlavor(f.Name)
 		demand, err := backchannel.GetResourceDemand("compute", resourceName)
 		if err != nil {
 			return nil, nil, fmt.Errorf("while collecting resource demand for compute/%s: %w", resourceName, err)
@@ -184,7 +187,7 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		if demand.OvercommitFactor != 1 && demand.OvercommitFactor != 0 {
 			return nil, nil, fmt.Errorf("overcommit on compute/%s is not supported", resourceName)
 		}
-		demandByFlavorName[f.Flavor.Name] = demand
+		demandByFlavorName[f.Name] = demand
 	}
 	logg.Debug("binpackable flavors: %#v", splitFlavors)
 	logg.Debug("demand for binpackable flavors: %#v", demandByFlavorName)
@@ -236,17 +239,15 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 	}
 
 	// during binpacking, place instances of large flavors first to achieve optimal results
-	slices.SortFunc(splitFlavors, func(lhs, rhs nova.FullFlavor) int {
+	slices.SortFunc(splitFlavors, func(lhs, rhs flavors.Flavor) int {
 		//NOTE: this returns `rhs-lhs` instead of `lhs-rhs` to achieve descending order
-		lf := lhs.Flavor
-		rf := rhs.Flavor
-		if lf.VCPUs != rf.VCPUs {
-			return rf.VCPUs - lf.VCPUs
+		if lhs.VCPUs != rhs.VCPUs {
+			return rhs.VCPUs - lhs.VCPUs
 		}
-		if lf.RAM != rf.RAM {
-			return rf.RAM - lf.RAM
+		if lhs.RAM != rhs.RAM {
+			return rhs.RAM - lhs.RAM
 		}
-		return rf.Disk - lf.Disk
+		return rhs.Disk - lhs.Disk
 	})
 
 	// if Nova can tell us where existing instances are running, we prefer this
@@ -258,12 +259,12 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 
 		// list all servers for this flavor, parsing only placement information from the result
 		listOpts := servers.ListOpts{
-			Flavor:     flavor.Flavor.ID,
+			Flavor:     flavor.ID,
 			AllTenants: true,
 		}
 		allPages, err := servers.List(p.NovaV2, listOpts).AllPages(ctx)
 		if err != nil {
-			return nil, nil, fmt.Errorf("while listing active instances for flavor %s: %w", flavor.Flavor.Name, err)
+			return nil, nil, fmt.Errorf("while listing active instances for flavor %s: %w", flavor.Name, err)
 		}
 		var instances []struct {
 			ID                 string                 `json:"id"`
@@ -272,7 +273,7 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		}
 		err = servers.ExtractServersInto(allPages, &instances)
 		if err != nil {
-			return nil, nil, fmt.Errorf("while listing active instances for flavor %s: %w", flavor.Flavor.Name, err)
+			return nil, nil, fmt.Errorf("while listing active instances for flavor %s: %w", flavor.Name, err)
 		}
 
 		for _, instance := range instances {
@@ -302,7 +303,7 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		}
 
 		if len(shadowedForThisFlavor) > 0 {
-			instancesPlacedOnShadowedHypervisors[flavor.Flavor.Name] = shadowedForThisFlavor
+			instancesPlacedOnShadowedHypervisors[flavor.Name] = shadowedForThisFlavor
 		}
 	}
 	logg.Debug("instances for split flavors placed on shadowed hypervisors: %v", instancesPlacedOnShadowedHypervisors)
@@ -312,7 +313,7 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 	for az, hypervisors := range hypervisorsByAZ {
 		canPlaceFlavor := make(map[string]bool)
 		for _, flavor := range splitFlavors {
-			canPlaceFlavor[flavor.Flavor.Name] = true
+			canPlaceFlavor[flavor.Name] = true
 		}
 
 		// phase 1: block existing usage
@@ -325,11 +326,11 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		for _, flavor := range splitFlavors {
 			// do not place instances that have already been placed in the simulation,
 			// as well as instances that run on hypervisors that do not participate in the binpacking simulation
-			placedUsage := hypervisors.PlacementCountForFlavor(flavor.Flavor.Name)
-			shadowedUsage := instancesPlacedOnShadowedHypervisors[flavor.Flavor.Name][az]
-			unplacedUsage := saturatingSub(demandByFlavorName[flavor.Flavor.Name].PerAZ[az].Usage, placedUsage+shadowedUsage)
+			placedUsage := hypervisors.PlacementCountForFlavor(flavor.Name)
+			shadowedUsage := instancesPlacedOnShadowedHypervisors[flavor.Name][az]
+			unplacedUsage := saturatingSub(demandByFlavorName[flavor.Name].PerAZ[az].Usage, placedUsage+shadowedUsage)
 			if !hypervisors.PlaceSeveralInstances(flavor, "used", coresDemand.OvercommitFactor, blockedCapacity, bb, unplacedUsage) {
-				canPlaceFlavor[flavor.Flavor.Name] = false
+				canPlaceFlavor[flavor.Name] = false
 			}
 		}
 
@@ -339,8 +340,8 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		blockedCapacity.LocalGB += instancesDemand.PerAZ[az].UnusedCommitments * maxRootDiskSize
 		logg.Debug("[%s] blockedCapacity in phase 2: %s", az, blockedCapacity.String())
 		for _, flavor := range splitFlavors {
-			if !hypervisors.PlaceSeveralInstances(flavor, "committed", coresDemand.OvercommitFactor, blockedCapacity, bb, demandByFlavorName[flavor.Flavor.Name].PerAZ[az].UnusedCommitments) {
-				canPlaceFlavor[flavor.Flavor.Name] = false
+			if !hypervisors.PlaceSeveralInstances(flavor, "committed", coresDemand.OvercommitFactor, blockedCapacity, bb, demandByFlavorName[flavor.Name].PerAZ[az].UnusedCommitments) {
+				canPlaceFlavor[flavor.Name] = false
 			}
 		}
 
@@ -350,8 +351,8 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		blockedCapacity.LocalGB += instancesDemand.PerAZ[az].PendingCommitments * maxRootDiskSize
 		logg.Debug("[%s] blockedCapacity in phase 3: %s", az, blockedCapacity.String())
 		for _, flavor := range splitFlavors {
-			if !hypervisors.PlaceSeveralInstances(flavor, "pending", coresDemand.OvercommitFactor, blockedCapacity, bb, demandByFlavorName[flavor.Flavor.Name].PerAZ[az].PendingCommitments) {
-				canPlaceFlavor[flavor.Flavor.Name] = false
+			if !hypervisors.PlaceSeveralInstances(flavor, "pending", coresDemand.OvercommitFactor, blockedCapacity, bb, demandByFlavorName[flavor.Name].PerAZ[az].PendingCommitments) {
+				canPlaceFlavor[flavor.Name] = false
 			}
 		}
 
@@ -361,15 +362,15 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		totalPlacedInstances := make(map[string]float64) // these two will diverge in the final round of placements
 		var splitFlavorsUsage nova.BinpackVector[uint64]
 		for _, flavor := range splitFlavors {
-			count := hypervisors.PlacementCountForFlavor(flavor.Flavor.Name)
-			initiallyPlacedInstances[flavor.Flavor.Name] = max(float64(count), 0.1)
+			count := hypervisors.PlacementCountForFlavor(flavor.Name)
+			initiallyPlacedInstances[flavor.Name] = max(float64(count), 0.1)
 			sumInitiallyPlacedInstances += count
-			totalPlacedInstances[flavor.Flavor.Name] = float64(count)
+			totalPlacedInstances[flavor.Name] = float64(count)
 			// The max(..., 0.1) is explained below.
 
-			splitFlavorsUsage.VCPUs += coresDemand.OvercommitFactor.ApplyInReverseTo(count * liquids.AtLeastZero(flavor.Flavor.VCPUs))
-			splitFlavorsUsage.MemoryMB += count * liquids.AtLeastZero(flavor.Flavor.RAM)
-			splitFlavorsUsage.LocalGB += count * liquids.AtLeastZero(flavor.Flavor.Disk)
+			splitFlavorsUsage.VCPUs += coresDemand.OvercommitFactor.ApplyInReverseTo(count * liquids.AtLeastZero(flavor.VCPUs))
+			splitFlavorsUsage.MemoryMB += count * liquids.AtLeastZero(flavor.RAM)
+			splitFlavorsUsage.LocalGB += count * liquids.AtLeastZero(flavor.Disk)
 		}
 
 		// for the upcoming final fill, we want to block capacity in such a way that
@@ -398,14 +399,14 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		// the placements (`totalPlacedInstances`).
 		for fillUp {
 			var (
-				bestFlavor *nova.FullFlavor
+				bestFlavor *flavors.Flavor
 				bestScore  = -1.0
 			)
 			for _, flavor := range splitFlavors {
-				if !canPlaceFlavor[flavor.Flavor.Name] {
+				if !canPlaceFlavor[flavor.Name] {
 					continue
 				}
-				score := (initiallyPlacedInstances[flavor.Flavor.Name]) / (2*totalPlacedInstances[flavor.Flavor.Name] + 1)
+				score := (initiallyPlacedInstances[flavor.Name]) / (2*totalPlacedInstances[flavor.Name] + 1)
 				// ^ This is why we adjusted all initiallyPlacedInstances[flavor.Name] = 0 to 0.1
 				// above. If the nominator of this fraction is 0 for multiple flavors, the first
 				// (biggest) flavor always wins unfairly. By adjusting to slightly away from zero,
@@ -421,9 +422,9 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 				break
 			} else {
 				if hypervisors.PlaceOneInstance(*bestFlavor, "padding", coresDemand.OvercommitFactor, blockedCapacity, bb) {
-					totalPlacedInstances[bestFlavor.Flavor.Name]++
+					totalPlacedInstances[bestFlavor.Name]++
 				} else {
-					canPlaceFlavor[bestFlavor.Flavor.Name] = false
+					canPlaceFlavor[bestFlavor.Name] = false
 				}
 			}
 		}
@@ -467,20 +468,20 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 			capacities[p.PooledInstancesResourceName][az] = pointerTo(azCapacity.IntoCapacityData("instances", float64(maxRootDiskSize), builder.InstancesSubcapacities))
 			capacities[p.PooledRAMResourceName][az] = pointerTo(azCapacity.IntoCapacityData("ram", float64(maxRootDiskSize), builder.RAMSubcapacities))
 			for _, flavor := range splitFlavors {
-				count := hypervisors.PlacementCountForFlavor(flavor.Flavor.Name)
-				capacities[p.PooledCoresResourceName][az].Capacity -= coresDemand.OvercommitFactor.ApplyInReverseTo(count * liquids.AtLeastZero(flavor.Flavor.VCPUs))
+				count := hypervisors.PlacementCountForFlavor(flavor.Name)
+				capacities[p.PooledCoresResourceName][az].Capacity -= coresDemand.OvercommitFactor.ApplyInReverseTo(count * liquids.AtLeastZero(flavor.VCPUs))
 				capacities[p.PooledInstancesResourceName][az].Capacity-- //TODO: not accurate when uint64(flavor.Disk) != maxRootDiskSize
-				capacities[p.PooledRAMResourceName][az].Capacity -= count * liquids.AtLeastZero(flavor.Flavor.RAM)
+				capacities[p.PooledRAMResourceName][az].Capacity -= count * liquids.AtLeastZero(flavor.RAM)
 			}
 		}
 	}
 
 	// compile result for split flavors
-	slices.SortFunc(splitFlavors, func(lhs, rhs nova.FullFlavor) int {
-		return strings.Compare(lhs.Flavor.Name, rhs.Flavor.Name)
+	slices.SortFunc(splitFlavors, func(lhs, rhs flavors.Flavor) int {
+		return strings.Compare(lhs.Name, rhs.Name)
 	})
 	for idx, flavor := range splitFlavors {
-		resourceName := p.FlavorAliases.LimesResourceNameForFlavor(flavor.Flavor.Name)
+		resourceName := p.FlavorAliases.LimesResourceNameForFlavor(flavor.Name)
 		capacities[resourceName] = make(core.PerAZ[core.CapacityData], len(hypervisorsByAZ))
 
 		for az, hypervisors := range hypervisorsByAZ {
@@ -494,7 +495,7 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 			}
 
 			capacities[resourceName][az] = &core.CapacityData{
-				Capacity:      hypervisors.PlacementCountForFlavor(flavor.Flavor.Name),
+				Capacity:      hypervisors.PlacementCountForFlavor(flavor.Name),
 				Subcapacities: builder.Subcapacities,
 			}
 		}
@@ -502,7 +503,7 @@ func (p *capacityNovaPlugin) Scrape(ctx context.Context, backchannel core.Capaci
 		// if shadowed hypervisors are still carrying instances of this flavor,
 		// increase the capacity accordingly to more accurately represent the
 		// free capacity on the unshadowed hypervisors
-		for az, shadowedCount := range instancesPlacedOnShadowedHypervisors[flavor.Flavor.Name] {
+		for az, shadowedCount := range instancesPlacedOnShadowedHypervisors[flavor.Name] {
 			if capacities[resourceName][az] == nil {
 				capacities[resourceName][az] = &core.CapacityData{
 					Capacity: shadowedCount,

internal/plugins/capacity_sapcc_ironic.go
@@ -67,6 +67,8 @@ func (p *capacitySapccIronicPlugin) Init(ctx context.Context, provider *gophercl
 	if err != nil {
 		return err
 	}
+	p.NovaV2.Microversion = "2.61" // to include extra specs in flavors.ListDetail()
+
 	p.IronicV1, err = openstack.NewBareMetalV1(provider, eo)
 	if err != nil {
 		return err

internal/plugins/nova.go
@@ -83,7 +83,7 @@ func (p *novaPlugin) Init(ctx context.Context, provider *gophercloud.ProviderCli
 	if err != nil {
 		return err
 	}
-	p.NovaV2.Microversion = "2.60" // to list server groups across projects and get all required server attributes
+	p.NovaV2.Microversion = "2.61" // to include extra specs in flavors.ListDetail()
 	cinderV3, err := openstack.NewBlockStorageV3(provider, eo)
 	if err != nil {
 		return err

internal/plugins/nova/binpack_simulation.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/sapcc/limes/internal/liquids"
 
+	"github.com/gophercloud/gophercloud/v2/openstack/compute/v2/flavors"
 	"github.com/gophercloud/gophercloud/v2/openstack/placement/v1/resourceproviders"
 	"github.com/sapcc/go-api-declarations/limes"
 	"github.com/sapcc/go-api-declarations/liquid"
@@ -159,9 +160,9 @@ func (h BinpackHypervisor) RenderDebugView(az limes.AvailabilityZone) {
 }
 
 // PlaceSeveralInstances calls PlaceOneInstance multiple times.
-func (hh BinpackHypervisors) PlaceSeveralInstances(ff FullFlavor, reason string, coresOvercommitFactor liquid.OvercommitFactor, blockedCapacity BinpackVector[uint64], bb BinpackBehavior, count uint64) (ok bool) {
+func (hh BinpackHypervisors) PlaceSeveralInstances(f flavors.Flavor, reason string, coresOvercommitFactor liquid.OvercommitFactor, blockedCapacity BinpackVector[uint64], bb BinpackBehavior, count uint64) (ok bool) {
 	for range count {
-		ok = hh.PlaceOneInstance(ff, reason, coresOvercommitFactor, blockedCapacity, bb)
+		ok = hh.PlaceOneInstance(f, reason, coresOvercommitFactor, blockedCapacity, bb)
 		if !ok {
 			// if we don't have space for this instance, we won't have space for any following ones
 			return false
@@ -172,7 +173,7 @@ func (hh BinpackHypervisors) PlaceSeveralInstances(ff FullFlavor, reason string,
 
 // PlaceOneInstance places a single instance of the given flavor using the vector-dot binpacking algorithm.
 // If the instance cannot be placed, false is returned.
-func (hh BinpackHypervisors) PlaceOneInstance(ff FullFlavor, reason string, coresOvercommitFactor liquid.OvercommitFactor, blockedCapacity BinpackVector[uint64], bb BinpackBehavior) (ok bool) {
+func (hh BinpackHypervisors) PlaceOneInstance(flavor flavors.Flavor, reason string, coresOvercommitFactor liquid.OvercommitFactor, blockedCapacity BinpackVector[uint64], bb BinpackBehavior) (ok bool) {
 	// This function implements the vector dot binpacking method described in [Mayank] (section III,
 	// subsection D, including the correction presented in the last paragraph of that subsection).
 	//
@@ -188,9 +189,9 @@ func (hh BinpackHypervisors) PlaceOneInstance(ff FullFlavor, reason string, core
 	// [Mayank]: https://www.it.iitb.ac.in/~sahoo/papers/cloud2011_mayank.pdf
 
 	vmSize := BinpackVector[uint64]{
-		VCPUs:    coresOvercommitFactor.ApplyInReverseTo(liquids.AtLeastZero(ff.Flavor.VCPUs)),
-		MemoryMB: liquids.AtLeastZero(ff.Flavor.RAM),
-		LocalGB:  liquids.AtLeastZero(ff.Flavor.Disk),
+		VCPUs:    coresOvercommitFactor.ApplyInReverseTo(liquids.AtLeastZero(flavor.VCPUs)),
+		MemoryMB: liquids.AtLeastZero(flavor.RAM),
+		LocalGB:  liquids.AtLeastZero(flavor.Disk),
 	}
 
 	// ensure that placing this instance does not encroach on the overall blocked capacity
@@ -202,7 +203,7 @@ func (hh BinpackHypervisors) PlaceOneInstance(ff FullFlavor, reason string, core
 	}
 	if !blockedCapacity.Add(vmSize).FitsIn(totalFree) {
 		logg.Debug("refusing to place %s with %s because of blocked capacity %s (total free = %s)",
-			ff.Flavor.Name, vmSize.String(), blockedCapacity.String(), totalFree.String())
+			flavor.Name, vmSize.String(), blockedCapacity.String(), totalFree.String())
 		return false
 	}
 
@@ -212,7 +213,7 @@ func (hh BinpackHypervisors) PlaceOneInstance(ff FullFlavor, reason string, core
 	)
 	for _, hypervisor := range hh {
 		// skip hypervisors that the flavor does not accept
-		if !ff.MatchesHypervisor(hypervisor.Match) {
+		if !FlavorMatchesHypervisor(flavor, hypervisor.Match) {
 			continue
 		}
 
@@ -240,11 +241,11 @@ func (hh BinpackHypervisors) PlaceOneInstance(ff FullFlavor, reason string, core
 	}
 
 	if bestNode == nil {
-		logg.Debug("refusing to place %s with %s because no node has enough space", ff.Flavor.Name, vmSize.String())
+		logg.Debug("refusing to place %s with %s because no node has enough space", flavor.Name, vmSize.String())
 		return false
 	} else {
 		bestNode.Instances = append(bestNode.Instances, BinpackInstance{
-			FlavorName: ff.Flavor.Name,
+			FlavorName: flavor.Name,
 			Size:       vmSize,
 			Reason:     reason,
 		})

internal/plugins/nova/flavor_selection.go
@@ -57,7 +57,7 @@ func (s FlavorSelection) matchesExtraSpecs(specs map[string]string) bool {
 
 // ForeachFlavor lists all public flavors matching this FlavorSelection, and
 // calls the given callback once for each of them.
-func (s FlavorSelection) ForeachFlavor(ctx context.Context, novaV2 *gophercloud.ServiceClient, action func(FullFlavor) error) error {
+func (s FlavorSelection) ForeachFlavor(ctx context.Context, novaV2 *gophercloud.ServiceClient, action func(flavors.Flavor) error) error {
 	opts := flavors.ListOpts{AccessType: flavors.AllAccess}
 	page, err := flavors.ListDetail(novaV2, &opts).AllPages(ctx)
 	if err != nil {
@@ -69,12 +69,8 @@ func (s FlavorSelection) ForeachFlavor(ctx context.Context, novaV2 *gophercloud.
 	}
 
 	for _, flavor := range allFlavors {
-		specs, err := flavors.ListExtraSpecs(ctx, novaV2, flavor.ID).Extract()
-		if err != nil {
-			return fmt.Errorf("while listing extra specs of public flavor %q: %w", flavor.Name, err)
-		}
-		if s.matchesExtraSpecs(specs) {
-			err = action(FullFlavor{flavor, specs})
+		if s.matchesExtraSpecs(flavor.ExtraSpecs) {
+			err = action(flavor)
 			if err != nil {
 				return err
 			}
@@ -83,14 +79,8 @@ func (s FlavorSelection) ForeachFlavor(ctx context.Context, novaV2 *gophercloud.
 	return nil
 }
 
-// FullFlavor is a flavor plus its set of extra specs.
-type FullFlavor struct {
-	Flavor     flavors.Flavor
-	ExtraSpecs map[string]string
-}
-
-// MatchesHypervisor returns true if instances of this flavor can be placed on the given hypervisor.
-func (f FullFlavor) MatchesHypervisor(mh MatchingHypervisor) bool {
+// FlavorMatchesHypervisor returns true if instances of this flavor can be placed on the given hypervisor.
+func FlavorMatchesHypervisor(f flavors.Flavor, mh MatchingHypervisor) bool {
 	// extra specs like `"trait:FOO": "required"` or `"trait:BAR": "forbidden"`
 	// are used by the Nova scheduler to ignore hypervisors that do not (or do)
 	// have the respective traits

internal/plugins/nova/flavor_translate.go
@@ -25,6 +25,7 @@ import (
 	"strings"
 
 	"github.com/gophercloud/gophercloud/v2"
+	"github.com/gophercloud/gophercloud/v2/openstack/compute/v2/flavors"
 	"github.com/sapcc/go-api-declarations/liquid"
 )
 
@@ -140,10 +141,10 @@ func (t FlavorTranslationTable) NovaQuotaNameForLimesResourceName(resourceName l
 // quotas, and returns the flavor names that Nova prefers for each.
 func (t FlavorTranslationTable) ListFlavorsWithSeparateInstanceQuota(ctx context.Context, computeV2 *gophercloud.ServiceClient) ([]string, error) {
 	var flavorNames []string
-	err := FlavorSelection{}.ForeachFlavor(ctx, computeV2, func(f FullFlavor) error {
+	err := FlavorSelection{}.ForeachFlavor(ctx, computeV2, func(f flavors.Flavor) error {
 		if f.ExtraSpecs["quota:separate"] == "true" {
-			flavorNames = append(flavorNames, f.Flavor.Name)
-			t.recordNovaPreferredName(f.Flavor.Name)
+			flavorNames = append(flavorNames, f.Name)
+			t.recordNovaPreferredName(f.Name)
 		}
 		return nil
 	})

commit c5ae73451c10d98b0600df946c99980f6ce8ccf6

fix enumeration of more than 1000 Nova hypervisors
Pagination was introduced in microversion 2.33, so our microversion upgrade
in 2fbae7df400a9c5c09d5db0c07d5d7b99eabf0e5 (PR #579) broke this
accidentally. This was not caught in QA because the QA regions are not
big enough to reach a 4-digit hypervisor count.

internal/plugins/nova/gophercloud_fixes.go
@@ -1,0 +1,74 @@
+/*******************************************************************************
+*
+* Copyright 2024 SAP SE
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You should have received a copy of the License along with this
+* program. If not, you may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+
+package nova
+
+import (
+	"net/http"
+
+	"github.com/gophercloud/gophercloud/v2"
+	"github.com/gophercloud/gophercloud/v2/openstack/compute/v2/hypervisors"
+	"github.com/gophercloud/gophercloud/v2/pagination"
+)
+
+// Like hypervisors.HypervisorPage, but fixes bug <https://github.com/gophercloud/gophercloud/issues/3222> by being a LinkedPageBase instead of a SinglePageBase.
+type hypervisorPage struct {
+	pagination.LinkedPageBase
+}
+
+// IsEmpty is required for LinkedPageBase to work.
+func (page hypervisorPage) IsEmpty() (bool, error) {
+	if page.StatusCode == http.StatusNoContent {
+		return true, nil
+	}
+
+	var data struct {
+		Hypervisors []struct{} `json:"hypervisors"`
+	}
+	err := page.ExtractInto(&data)
+	return len(data.Hypervisors) == 0, err
+}
+
+// NextPageURL is required for LinkedPageBase to work.
+func (page hypervisorPage) NextPageURL() (string, error) {
+	var s struct {
+		Links []gophercloud.Link `json:"hypervisors_links"`
+	}
+	err := page.ExtractInto(&s)
+	if err != nil {
+		return "", err
+	}
+	return gophercloud.ExtractNextURL(s.Links)
+}
+
+// Like hypervisors.List(), but fixes bug <https://github.com/gophercloud/gophercloud/issues/3222>.
+func hypervisorsList(client *gophercloud.ServiceClient, opts hypervisors.ListOptsBuilder) pagination.Pager {
+	url := client.ServiceURL("os-hypervisors", "detail")
+	if opts != nil {
+		query, err := opts.ToHypervisorListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+
+	return pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
+		return hypervisorPage{pagination.LinkedPageBase{PageResult: r}}
+	})
+}

internal/plugins/nova/hypervisor_selection.go
@@ -55,14 +55,14 @@ type HypervisorSelection struct {
 // HypervisorSelection, and calls the given callback once for each of them.
 func (s HypervisorSelection) ForeachHypervisor(ctx context.Context, novaV2, placementV1 *gophercloud.ServiceClient, action func(MatchingHypervisor) error) error {
 	// enumerate hypervisors
-	page, err := hypervisors.List(novaV2, nil).AllPages(ctx)
+	page, err := hypervisorsList(novaV2, nil).AllPages(ctx)
 	if err != nil {
 		return fmt.Errorf("while listing hypervisors: %w", err)
 	}
 	var hypervisorData struct {
 		Hypervisors []Hypervisor `json:"hypervisors"`
 	}
-	err = page.(hypervisors.HypervisorPage).ExtractInto(&hypervisorData)
+	err = page.(hypervisorPage).ExtractInto(&hypervisorData)
 	if err != nil {
 		return fmt.Errorf("while listing hypervisors: %w", err)
 	}