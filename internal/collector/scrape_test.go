e9e681ec4a1a39a49e17a8af0646b4693a9dd4fb
VoigtS <stefanv73@web.de>
2024-11-29 15:54:42 +0100

    Fix linter

--- internal/collector/scrape_test.go
+++ internal/collector/scrape_test.go
@@ -702,5 +702,4 @@ func Test_TopologyScrapes(t *testing.T) {
 	// negative: reject liquid initialization with invalid topologies
 	plugin.LiquidServiceInfo.Resources = map[liquid.ResourceName]liquid.ResourceInfo{"capacity": {Topology: "invalidAZ1"}, "things": {Topology: "invalidAZ2"}}
 	mustFailT(t, job.ProcessOne(s.Ctx, withLabel), errors.New("during resource scrape of project germany/dresden: invalid toplogy: invalidAZ1 on resource: capacity\ninvalid toplogy: invalidAZ2 on resource: things"))
-
 }
