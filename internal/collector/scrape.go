5f5386f7c825fbeb541e71a229614197747730d8
VoigtS <stefanv73@web.de>
2024-12-02 11:33:21 +0100

    Simplify backendquota reset on non AZ separated resources

--- internal/collector/scrape.go
+++ internal/collector/scrape.go
@@ -64,12 +64,6 @@ var (
 		WHERE pr.service_id = $1
 	`)
 
-	resetAZBackendQuotas = sqlext.SimplifyWhitespace(`
-		UPDATE project_az_resources 
-			SET backend_quota = NULL
-		WHERE resource_id = $1 and backend_quota IS NOT NULL
-	`)
-
 	writeResourceScrapeSuccessQuery = sqlext.SimplifyWhitespace(`
 		UPDATE project_services SET
 			-- timing information
@@ -318,10 +312,7 @@ func (c *Collector) writeResourceScrapeResult(dbDomain db.Domain, dbProject db.P
 				}
 				// reset backendQuota entries for topology changes
 				if resInfo.Topology != liquid.AZSeparatedResourceTopology {
-					_, err = c.DB.Exec(resetAZBackendQuotas, azRes.ResourceID)
-					if err != nil {
-						return fmt.Errorf("AZ backend quota reset for resource %s failed", resourceName)
-					}
+					azRes.BackendQuota = nil
 				}
 
 				// warn when the backend is inconsistent with itself
