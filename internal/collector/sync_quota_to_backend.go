bcf81ccd271aaff481053b2a9f8d4f36aacf732d
Stefan Majewsky <stefan.majewsky@sap.com>
2024-12-16 15:20:18 +0100

    fix handling of project_resources.quota for AZSeparatedResourceTopology

--- internal/collector/sync_quota_to_backend.go
+++ internal/collector/sync_quota_to_backend.go
@@ -93,10 +93,12 @@ func (c *Collector) processQuotaSyncTask(ctx context.Context, srv db.ProjectServ
 }
 
 var (
+	// NOTE: This query does not use `AND quota IS NOT NULL` to filter out NoQuota resources
+	// because it would also filter out resources with AZSeparatedResourceTopology.
 	quotaSyncSelectQuery = sqlext.SimplifyWhitespace(`
 		SELECT id, name, backend_quota, quota
 		  FROM project_resources
-		 WHERE service_id = $1 AND quota IS NOT NULL
+		 WHERE service_id = $1
 	`)
 	azQuotaSyncSelectQuery = sqlext.SimplifyWhitespace(`
 		SELECT az, backend_quota, quota
@@ -140,17 +142,22 @@ func (c *Collector) performQuotaSync(ctx context.Context, srv db.ProjectService,
 	var azSeparatedResourceIDs []db.ProjectResourceID
 	err := sqlext.ForeachRow(c.DB, quotaSyncSelectQuery, []any{srv.ID}, func(rows *sql.Rows) error {
 		var (
-			resourceID   db.ProjectResourceID
-			resourceName liquid.ResourceName
-			currentQuota *int64
-			targetQuota  uint64
+			resourceID     db.ProjectResourceID
+			resourceName   liquid.ResourceName
+			currentQuota   *int64
+			targetQuotaPtr *uint64
 		)
-		err := rows.Scan(&resourceID, &resourceName, &currentQuota, &targetQuota)
+		err := rows.Scan(&resourceID, &resourceName, &currentQuota, &targetQuotaPtr)
 		if err != nil {
 			return err
 		}
 
 		resInfo := c.Cluster.InfoForResource(srv.Type, resourceName)
+		if !resInfo.HasQuota {
+			return nil
+		}
+
+		var targetQuota uint64
 		if resInfo.Topology == liquid.AZSeparatedResourceTopology {
 			// for AZSeparatedResourceTopology, project_resources.quota is effectively empty (always set to zero)
 			// and `targetQuota` needs to be computed by summing over project_az_resources.quota
@@ -183,6 +190,12 @@ func (c *Collector) performQuotaSync(ctx context.Context, srv db.ProjectService,
 			if err != nil {
 				return err
 			}
+		} else {
+			// for anything except AZSeparatedResourceTopology, the total target quota is just what we have in project_resources.quota
+			if targetQuotaPtr == nil {
+				return fmt.Errorf("found unexpected NULL value in project_resources.quota for id = %d", resourceID)
+			}
+			targetQuota = *targetQuotaPtr
 		}
 
 		targetQuotasInDB[resourceName] = targetQuota
