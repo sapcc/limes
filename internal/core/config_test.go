e79881ff62d6bda396b48e0c71c45f1667a376ed
Stefan Majewsky <stefan.majewsky@sap.com>
2025-07-17 11:45:54 +0200

    internal/core: move tests into a separate package

--- internal/core/config_test.go
+++ internal/core/config_test.go
@@ -1,27 +1,29 @@
 // SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company
 // SPDX-License-Identifier: Apache-2.0
 
-package core
+package core_test
 
 import (
 	"testing"
 
 	"github.com/sapcc/go-bits/assert"
+
+	"github.com/sapcc/limes/internal/core"
 )
 
 func TestFilterDomains(t *testing.T) {
-	cfg := DiscoveryConfiguration{
+	cfg := core.DiscoveryConfiguration{
 		IncludeDomainRx: "foo",
 		ExcludeDomainRx: "2$",
 	}
 
-	input := []KeystoneDomain{
+	input := []core.KeystoneDomain{
 		{Name: "bar1"},
 		{Name: "bar2"},
 		{Name: "foo1"},
 		{Name: "foo2"},
 	}
-	expected := []KeystoneDomain{
+	expected := []core.KeystoneDomain{
 		{Name: "foo1"},
 	}
 	assert.DeepEqual(t, "filtered domains", cfg.FilterDomains(input), expected)
