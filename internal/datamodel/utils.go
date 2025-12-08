67e7c00fb5d6f9fc1a50f339caadde6e867e03f5
Daniel Wagner <daniel.wagner03@sap.com>
2025-12-08 14:06:44 +0100

    datamodel: rewrite complex conditionals for clarity
    This rewrite also covers all corner cases correctly, even ones for which
    these functions are currently not called because no matching records
    exist in project_az_resources et al.

--- internal/datamodel/utils.go
+++ internal/datamodel/utils.go
@@ -9,14 +9,33 @@ import "github.com/sapcc/go-api-declarations/liquid"
 // More specifically, for az-separated case, there is no "total" AZ value necessary.
 // Unknown never gets a valid value.
 func AZHasQuotaForTopology(topology liquid.Topology, az liquid.AvailabilityZone) bool {
-	return az != liquid.AvailabilityZoneUnknown && (topology != liquid.AZSeparatedTopology || az != liquid.AvailabilityZoneTotal)
+	if az == liquid.AvailabilityZoneUnknown {
+		return false
+	}
+	switch topology {
+	case liquid.FlatTopology:
+		return !az.IsReal()
+	case liquid.AZAwareTopology:
+		return true
+	case liquid.AZSeparatedTopology:
+		return az.IsReal()
+	default:
+		// require an explicit update to this function when new topologies are added
+		panic("do not know how to handle topology: " + string(topology))
+	}
 }
 
 // AZHasBackendQuotaForTopology returns true if the given availability zone can have a backend quota for the given topology.
 // This behaves almost similar to AZHasQuotaForTopology, but for non-az-separated topologies, only the "total" AZ value is valid.
 // Unknown never gets a valid value.
 func AZHasBackendQuotaForTopology(topology liquid.Topology, az liquid.AvailabilityZone) bool {
-	return az != liquid.AvailabilityZoneUnknown &&
-		((topology == liquid.AZSeparatedTopology && az != liquid.AvailabilityZoneTotal) ||
-			(topology != liquid.AZSeparatedTopology && az == liquid.AvailabilityZoneTotal))
+	switch topology {
+	case liquid.FlatTopology, liquid.AZAwareTopology:
+		return az == liquid.AvailabilityZoneTotal
+	case liquid.AZSeparatedTopology:
+		return az.IsReal()
+	default:
+		// require an explicit update to this function when new topologies are added
+		panic("do not know how to handle topology: " + string(topology))
+	}
 }
