e3ecefcf2e06e5852b28b327c4488b0046115ee9
Daniel Wagner <daniel.wagner03@sap.com>
2026-03-06 14:37:16 +0100

    switch liquid designate to use prometheus queries for usage

--- internal/liquids/designate/client.go
+++ internal/liquids/designate/client.go
@@ -8,8 +8,6 @@ import (
 
 	"github.com/gophercloud/gophercloud/v2"
 	"github.com/gophercloud/gophercloud/v2/openstack"
-	"github.com/gophercloud/gophercloud/v2/openstack/dns/v2/zones"
-	"github.com/gophercloud/gophercloud/v2/pagination"
 )
 
 // Client is a gophercloud.ServiceClient for the Designate API.
@@ -48,47 +46,4 @@ func (c *Client) setQuota(ctx context.Context, projectUUID string, qs quotaSet) 
 
 	_, _, err := gophercloud.ParseResponse(c.Patch(ctx, url, qs, nil, &opts)) //nolint:bodyclose
 	return err
-}
-
-func (c *Client) listZoneIDs(ctx context.Context, projectUUID string) ([]string, error) {
-	pager := zones.List(c.ServiceClient, zones.ListOpts{})
-	pager.Headers = map[string]string{
-		"X-Auth-All-Projects":    "false",
-		"X-Auth-Sudo-Project-Id": projectUUID,
-	}
-
-	var ids []string
-	err := pager.EachPage(ctx, func(ctx context.Context, page pagination.Page) (bool, error) {
-		zones, err := zones.ExtractZones(page)
-		if err != nil {
-			return false, err
-		}
-		for _, zone := range zones {
-			ids = append(ids, zone.ID)
-		}
-		return true, nil
-	})
-	return ids, err
-}
-
-func (c *Client) countZoneRecordsets(ctx context.Context, projectUUID, zoneID string) (uint64, error) {
-	url := c.ServiceURL("zones", zoneID, "recordsets")
-	url += "?limit=1" // do not need all data about all recordsets, just the total count
-	opts := gophercloud.RequestOpts{
-		MoreHeaders: map[string]string{
-			"X-Auth-All-Projects":    "false",
-			"X-Auth-Sudo-Project-Id": projectUUID,
-		},
-	}
-
-	var r gophercloud.Result
-	_, r.Header, r.Err = gophercloud.ParseResponse(c.Get(ctx, url, &r.Body, &opts)) //nolint:bodyclose
-
-	var data struct {
-		Metadata struct {
-			Count uint64 `json:"total_count"`
-		} `json:"metadata"`
-	}
-	err := r.ExtractInto(&data)
-	return data.Metadata.Count, err
 }
