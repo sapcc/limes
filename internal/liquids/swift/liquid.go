f2f2c66c1dafa7eb72bd0ed1a223f200e7c6af11
Stefan Majewsky <stefan.majewsky@sap.com>
2026-04-19 13:27:53 +0200

    bump schwift to v2.1.0
    The module path change requires manual intervention.

--- internal/liquids/swift/liquid.go
+++ internal/liquids/swift/liquid.go
@@ -12,11 +12,11 @@ import (
 	"github.com/gophercloud/gophercloud/v2"
 	"github.com/gophercloud/gophercloud/v2/openstack"
 	. "github.com/majewsky/gg/option"
-	"github.com/majewsky/schwift/v2"
-	"github.com/majewsky/schwift/v2/gopherschwift"
 	"github.com/sapcc/go-api-declarations/liquid"
 	"github.com/sapcc/go-bits/logg"
 	"github.com/sapcc/go-bits/respondwith"
+	"go.xyrillian.de/schwift/v2"
+	"go.xyrillian.de/schwift/v2/gopherschwift"
 )
 
 // Logic implements the liquidapi.Logic interface for Swift.
