515d37854cf8c85dc7d69bf2a351bd3da62f4197
Daniel Wagner <daniel.wagner03@sap.com>
2026-06-22 13:34:09 +0200

    add API report structure

--- internal/api/api_v2/cluster.go
+++ internal/api/api_v2/cluster.go
@@ -3,6 +3,55 @@
 
 package api_v2
 
-import "github.com/sapcc/go-api-declarations/opts"
+import (
+	"net/http"
 
-var _ = opts.ParseQueryString[struct{}]
+	"github.com/sapcc/go-api-declarations/opts"
+	"github.com/sapcc/go-bits/gopherpolicy"
+	"github.com/sapcc/go-bits/httpapi"
+
+	"github.com/sapcc/limes/internal/api/reports_v2"
+	"github.com/sapcc/limes/internal/apideclarations/apiv2/common"
+	ratesv2 "github.com/sapcc/limes/internal/apideclarations/apiv2/rates"
+	resourcesv2 "github.com/sapcc/limes/internal/apideclarations/apiv2/resources"
+)
+
+// handleGetResourcesCluster handles GET /resources/v2/cluster.
+func (p *v2Provider) handleGetResourcesCluster(r *http.Request, token *gopherpolicy.Token) (_ resourcesv2.ClusterGetResponse, err error) {
+	httpapi.IdentifyEndpoint(r, "/resources/v2/cluster")
+	none := resourcesv2.ClusterGetResponse{}
+
+	err = token.Enforce("v2:cluster:report")
+	if err != nil {
+		return none, err
+	}
+	options, err := opts.ParseQueryString[common.ClusterResourceReportOpts](r.URL.Query())
+	if err != nil {
+		return none, err
+	}
+	_, err = reports_v2.FilterFromResourceOpts(p.Cluster, options.ResourceReportOpts)
+	if err != nil {
+		return none, err
+	}
+	return none, nil
+}
+
+// handleGetRatesCluster handles GET /rates/v2/cluster.
+func (p *v2Provider) handleGetRatesCluster(r *http.Request, token *gopherpolicy.Token) (_ ratesv2.ClusterGetResponse, err error) {
+	httpapi.IdentifyEndpoint(r, "/rates/v2/cluster")
+	none := ratesv2.ClusterGetResponse{}
+
+	err = token.Enforce("v2:cluster:report")
+	if err != nil {
+		return none, err
+	}
+	options, err := opts.ParseQueryString[common.ClusterRateReportOpts](r.URL.Query())
+	if err != nil {
+		return none, err
+	}
+	_, err = reports_v2.FilterFromRateOpts(p.Cluster, options.RateReportOpts)
+	if err != nil {
+		return none, err
+	}
+	return none, nil
+}
