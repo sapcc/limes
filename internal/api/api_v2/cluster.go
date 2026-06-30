f39753fd56e3e2c3afb533867415d6371f4933a5
Stefan Majewsky <stefan.majewsky@sap.com>
2026-06-30 16:45:33 +0200

    vendor github.com/sapcc/go-api-declarations/opts early
    To avoid repeated merge conflicts on #904 from library updates.

---
+++ internal/api/api_v2/cluster.go
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company
+// SPDX-License-Identifier: Apache-2.0
+
+package api_v2
+
+import "github.com/sapcc/go-api-declarations/opts"
+
+var _ = opts.ParseQueryString[struct{}]
